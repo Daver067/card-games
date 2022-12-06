d89ad0a3b50612517f1ccb3d14110c43d438082a
Dave <davediemert_88@hotmail.com>
2022-12-05 19:12:13 -0800

    working the merge

--- src/scripts/matchingGame/matchIndex.js
+++ src/scripts/matchingGame/matchIndex.js
@@ -22,6 +22,7 @@ const matchGame = {
 
     matchGame.deck.cards.forEach((cardInDeck) => {
       cardInDeck.matched = false;
+      cardInDeck.card.style.positon = "relative";
       matchGame.addflip(cardInDeck);
       testFlop.appendChild(cardInDeck.card);
     });
